334720b4c97f4e8f1358c9f04833b30a2c126bc2
Doug Alderman <agnadcp-d1@yahoo.com>
2016-04-27 10:57:31 -0600

    4/27/16 changes morning

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,20 +1,33 @@
 angular.module('myPortfolio')
 .controller('mainCtrl', function ($scope) {
-    
-   /* $( document ).ready(function(){
+	
+	$(document).ready(function() {
+
+		/* $("ul li a").click(function(){
+			return false;  // prevent hash from showing in address bar	
+		}); */
 		
-		$scope.showNavbar = false;
-        
-          
-        $('.button-collapse').sideNav();
-        
-  		var options = [
-    		{selector: '#top', offset: 300, callback: function() {
-      				$scope.showNavbar = true;	
-    			}
-			},
-  		];
-  		Materialize.scrollFire(options);      
-    }) */
+		$.localScroll();
+
+	});
+	
+	$scope.contact = {};
+    $scope.emailMatchError = false;
+     
+  /*  if ($scope.reloadScreen) {
+            location.reload();
+            $scope.reloadScreen = false;
+    } */
     
+    $scope.contactForm = function() {
+        console.log('in contact form')
+        console.log('$scope.contact', $scope.contact);
+        if ($scope.contact.email === $scope.contact.emailAgain) {
+            $scope.emailMatchError = false;
+            // process form
+        }
+        else {
+            $scope.emailMatchError = true;
+        }
+    }  
 });
